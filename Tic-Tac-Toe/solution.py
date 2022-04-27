5353e2c7473c4b0995d1d20e72efcd6119135ba5
Deaton879 <Deaton879@live.com>
2022-04-27 15:43:36 -0400

    Win cases finished, comments added. Game complete

--- Tic-Tac-Toe/solution.py
+++ Tic-Tac-Toe/solution.py
@@ -4,36 +4,74 @@
 #-----------------------------
 
 def main():
+    # Set inital game-state
     game_over = False
-    player = 'x'
+    player = 'X'
     board = make_board()
+    timer = 0
+
+    # Game loop 
     while (game_over == False):
+        timer += 1
+        
+        if (timer >= 10):
+            # All spaces will have been filled, so game loop will end
+            break
+
+        player = change_player(player)
+        
         draw_board(board)
+        
         prompt_move(player, board)
-        if (player == "x"):
-            player = "o"
-        else:
-            player = "x"
+        
+        game_over = check_win(board, player)
+
+    # Draw final board
+    draw_board(board)
+    # Determine the right message based on timer
+    if (timer < 10):
+        print(f"Congratulations player: {player}. You win!")
+    else:
+        print("Game over. It's a draw!")
+        
 
 def make_board():
     board = []
-    for spot in range(9):
+    for num in range(9):
         board.append(" ")
     return board
 
+def change_player(player):
+        if (player == "X"):
+            return "O"
+        else:
+            return "X"
+
+def check_win(board, player):
+    # player symbol has to match one of the combinations to trigger True
+    return (player == board[0] == board[1] == board[2] or
+            player == board[3] == board[4] == board[5] or
+            player == board[6] == board[7] == board[8] or
+            player == board[0] == board[3] == board[6] or
+            player == board[1] == board[4] == board[7] or
+            player == board[2] == board[5] == board[8] or
+            player == board[0] == board[4] == board[8] or
+            player == board[2] == board[4] == board[6])
+
 def draw_board(board):
-    print("  a b c")
     print(" +-----+")
-    print(f"1|{board[0]}|{board[1]}|{board[2]}|")
+    print(f" |{board[0]}|{board[1]}|{board[2]}|")
     print(" |-+-+-|")
-    print(f"2|{board[3]}|{board[4]}|{board[5]}|")
+    print(f" |{board[3]}|{board[4]}|{board[5]}|")
     print(" |-+-+-|")
-    print(f"3|{board[6]}|{board[7]}|{board[8]}|")
+    print(f" |{board[6]}|{board[7]}|{board[8]}|")
     print(" +-----+")
 
 def prompt_move(player, board):
-    spot = int(input(f"{player}, please make a move (1-9, starting top-left): "))
+    spot = int(input(f"Player: {player}, please make a move (enter 1-9. Grid starting top-left, ending bottom-right): "))
+    # Make sure space is not already taken
     while (board[spot - 1] != " "):
+        # if it is, prompt again (until empty space is chosen)
         spot = int(input(f"{player}, try again: "))    
     board[spot-1] = player
 
